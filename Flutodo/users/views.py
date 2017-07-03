b0a6fd56ef4eb320ead12a79ecc5a59f6e37f750
Rene Montilva <renemontilva@gmail.com>
2017-07-03 16:22:43 -0400

    added signup functions

---
+++ Flutodo/users/views.py
@@ -0,0 +1,11 @@
+from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse_lazy
+
+
+class SignUp(generic.CreateView):
+    model = User
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('login')
+    form_class = UserCreationForm
